28770645ebe9dd11dd654be75af2143098d77b3b
Woody <nz.wgkim@gmail.com>
2023-04-12 07:14:09 +1200

    Clone completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,324 @@
 import 'package:flutter/material.dart';
 
+const bgColor = Color(0xff1f1f1f);
+const cardColorYellow = Color(0xfffef754);
+const cardColorPurple = Color(0xff9c6bce);
+const cardColorGreen = Color(0xffbcee4b);
+
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: bgColor,
+        body: SingleChildScrollView(
+          child: SafeArea(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: const [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: UpmostPart(),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ScheduleCard(
+                bgColor: cardColorYellow,
+                startHour: 11,
+                startMinute: 30,
+                endHour: 12,
+                endMinute: 20,
+                upperContents: 'DESIGN',
+                lowerContents: 'MEETING',
+                persionlist: 1,
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              ScheduleCard(
+                  bgColor: cardColorPurple,
+                  startHour: 12,
+                  startMinute: 35,
+                  endHour: 14,
+                  endMinute: 10,
+                  upperContents: 'DAILY',
+                  lowerContents: 'PROJECT',
+                  persionlist: 2),
+              SizedBox(
+                height: 5,
+              ),
+              ScheduleCard(
+                  bgColor: cardColorGreen,
+                  startHour: 15,
+                  startMinute: 0,
+                  endHour: 16,
+                  endMinute: 30,
+                  upperContents: 'WEEKLY',
+                  lowerContents: 'PLANNING',
+                  persionlist: 3),
+            ],
+          )),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class UpmostPart extends StatelessWidget {
+  const UpmostPart({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Image.asset(
+            'assets/ui_clone.png',
+            height: 40,
+          ),
+          const Icon(
+            Icons.add,
+            size: 30,
+            color: Colors.white,
+          )
+        ]),
+        const SizedBox(
+          height: 18,
+        ),
+        const Text(
+          'MONDAY 16',
+          textAlign: TextAlign.left,
+          style: TextStyle(
+              fontSize: 12, fontWeight: FontWeight.w600, color: Colors.white54),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Row(
+          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              'TODAY',
+              style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white.withOpacity(0.9)),
+            ),
+            const SizedBox(
+              width: 4,
+            ),
+            const Icon(
+              Icons.circle,
+              size: 8,
+              color: Color(0xffb12680),
+            ),
+            const SizedBox(
+              width: 4,
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              '17 18 19 2',
+              style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white.withOpacity(0.6)),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class ScheduleCard extends StatelessWidget {
+  final Color bgColor;
+  final int startHour, startMinute, endHour, endMinute;
+  final String upperContents, lowerContents;
+  // final List<String> persons;
+  final int persionlist;
+
+  const ScheduleCard(
+      {super.key,
+      required this.bgColor,
+      required this.startHour,
+      required this.startMinute,
+      required this.endHour,
+      required this.endMinute,
+      required this.upperContents,
+      required this.lowerContents,
+      required this.persionlist});
+
+  @override
+  Widget build(BuildContext context) {
+    const TextStyle timeStyleHour =
+        TextStyle(fontSize: 16, fontWeight: FontWeight.w600);
+    const TextStyle timeStyleMinute =
+        TextStyle(fontSize: 12, fontWeight: FontWeight.w400);
+
+    const TextStyle contentsTextStyle =
+        TextStyle(fontSize: 45, fontWeight: FontWeight.w500, height: 0.8);
+
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(10, 0, 10, 3),
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        decoration: BoxDecoration(
+            color: bgColor, borderRadius: BorderRadius.circular(30)),
+        child: Column(
+          children: [
+            // const SizedBox(
+            //   height: 1,
+            // ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        startHour.toString(),
+                        style: timeStyleHour,
+                      ),
+                      Text(
+                        startMinute.toString(),
+                        style: timeStyleMinute,
+                      ),
+                      // const Icon(Icons.vertical_align_bottom),
+                      // const Text('|',style: TextStyle(fontSize: ),),
+                      const SizedBox(
+                        height: 20,
+                        child: VerticalDivider(
+                          color: Colors.black,
+                          thickness: 1,
+                        ),
+                      ),
+                      Text(
+                        endHour.toString(),
+                        style: timeStyleHour,
+                      ),
+                      Text(
+                        endMinute.toString(),
+                        style: timeStyleMinute,
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  width: 18,
+                ),
+                Container(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        upperContents,
+                        style: contentsTextStyle,
+                      ),
+                      Text(
+                        lowerContents,
+                        style: contentsTextStyle,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                const SizedBox(
+                  width: 50,
+                ),
+                if (persionlist == 1)
+                  AlexRow()
+                else if (persionlist == 2)
+                  MeRow()
+                else
+                  DenRow(),
+              ],
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Widget AlexRow() {
+    final style = TextStyle(
+        color: Colors.black.withOpacity(0.4), fontWeight: FontWeight.w600);
+    return Row(children: [
+      Text(
+        'ALEX',
+        style: style,
+      ),
+      const SizedBox(
+        width: 16,
+      ),
+      Text('HELENA', style: style),
+      const SizedBox(
+        width: 16,
+      ),
+      Text(
+        'NANA',
+        style: style,
+      )
+    ]);
+  }
+
+  Widget MeRow() {
+    final style = TextStyle(
+        color: Colors.black.withOpacity(0.4), fontWeight: FontWeight.w600);
+    return Row(children: [
+      const Text(
+        'ME',
+        style: TextStyle(fontWeight: FontWeight.w600),
+      ),
+      const SizedBox(
+        width: 16,
+      ),
+      Text('RICHARDO', style: style),
+      const SizedBox(
+        width: 16,
+      ),
+      Text('CIRY', style: style),
+      const SizedBox(
+        width: 16,
+      ),
+      Text('+4', style: style)
+    ]);
+  }
+
+  Widget DenRow() {
+    final style = TextStyle(
+        color: Colors.black.withOpacity(0.4), fontWeight: FontWeight.w600);
+    return Row(children: [
+      Text('DEN', style: style),
+      const SizedBox(
+        width: 16,
+      ),
+      Text('NANA', style: style),
+      const SizedBox(
+        width: 16,
+      ),
+      Text(
+        'MARK',
+        style: style,
+      )
+    ]);
   }
 }
